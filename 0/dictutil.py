78e4292ab878770be09d5272ba74c9627b6e0ea9
Randy Song <rsong@toasttab.com>
2020-12-04 15:10:42 -0500

    new structure

--- 0/dictutil.py
+++
@@ -1,5 +0,0 @@
-def dict2list(dct,keylist): return [dct[x] for x in keylist]
-
-def list2dict(L, keylist): return {keylist[x]:L[x] for x in range(len(keylist))}
-
-def listrange2dict(L): return {x:L[x] for x in range(len(L))}
