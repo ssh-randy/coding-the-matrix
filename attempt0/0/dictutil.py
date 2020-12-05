6492a10eb8d25c3be078f34d85b49cab32154ad7
Randy Song <rsong@toasttab.com>
2020-12-05 14:32:21 -0500

    add 0th attempt

---
+++ attempt0/0/dictutil.py
@@ -0,0 +1,5 @@
+def dict2list(dct,keylist): return [dct[x] for x in keylist]
+
+def list2dict(L, keylist): return {keylist[x]:L[x] for x in range(len(keylist))}
+
+def listrange2dict(L): return {x:L[x] for x in range(len(L))}
